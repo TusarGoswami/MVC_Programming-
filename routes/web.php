4fe159142923367e4bf3fd27ea948242bb0eaf74
Tusar Goswami <tusarg937@gmail.com>
2026-03-11 16:05:32 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+// Basic Routing
 Route::get('/', function () {
     return "Hello To Laravel Class";
+});
+
+Route :: get('/courses', function() {
+    $course = ['PHP', 'Java', 'Python', 'C++']; 
+    foreach($course as $c){
+        echo "$c". "<br>";
+    }
+});
+
+Route :: get('/display', function() {
+    for($i=0; $i<10; $i++){
+        echo "$i". "<br>";
+    }
+});
+
+//Required Routing
+Route :: get('/user/{name}', function($name) {
+    return "Welcome $name";
 });
